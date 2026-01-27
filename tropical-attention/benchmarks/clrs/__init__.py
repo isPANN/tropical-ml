72300c54cf66f73c4dd3ee42be1dfa6fb8b6dcd7
Xiwei Pan <xiwei.pan@connect.hkust-gz.edu.cn>
2026-01-28 02:07:52 +0800

    update

--- tropical-attention/benchmarks/clrs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CLRS Benchmark integration for Tropical Attention."""
-
-from .processor import TropicalProcessor
-from .model import TropicalCLRSModel
-
-__all__ = ["TropicalProcessor", "TropicalCLRSModel"]
